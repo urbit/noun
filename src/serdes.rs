54f84abfd30aa502b3c4f8fcfc05f6a290b1a855
Peter McEvoy <peter@tlon.io>
2022-06-15 12:24:39 -0700

    Add Error associated type to Jam and Cue traits

--- src/serdes.rs
+++ src/serdes.rs
@@ -2,12 +2,16 @@ pub trait Jam
 where
     Self: Sized,
 {
-    fn jam(self) -> Result<Vec<u8>, Self>;
+    type Error;
+
+    fn jam(self) -> Result<Vec<u8>, Self::Error>;
 }
 
 pub trait Cue
 where
     Self: Sized,
 {
-    fn cue(jammed_val: Vec<u8>) -> Result<Self, Vec<u8>>;
+    type Error;
+
+    fn cue(jammed_val: Vec<u8>) -> Result<Self, Self::Error>;
 }
