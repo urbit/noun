5dafaba1269cc6167ab040be678e2e4ac0012f6a
Peter McEvoy <peter@tlon.io>
2022-06-15 13:32:19 -0700

    Add IntoNoun trait bounds to Atom and Cell traits

--- src/atom.rs
+++ src/atom.rs
@@ -1,16 +1,14 @@
-use crate::{cell::Cell, Noun};
+use crate::{cell::Cell, IntoNoun, Noun};
 
 pub trait Atom<C, N>
 where
     C: Cell<Self, N>,
     N: Noun<Self, C>,
-    Self: Sized,
+    Self: IntoNoun<Self, C, N> + Sized,
 {
     type Error;
 
     fn as_vec(&self) -> Vec<u8>;
 
-    fn as_u64(&self) -> Result<u64, Self::Error>;
-
-    fn into_noun(self) -> N;
+    fn as_u64(&self) -> Result<u64, <Self as Atom<C, N>>::Error>;
 }
--- src/cell.rs
+++ src/cell.rs
@@ -1,14 +1,12 @@
-use crate::{atom::Atom, Noun};
+use crate::{atom::Atom, IntoNoun, Noun};
 
 pub trait Cell<A, N>
 where
     A: Atom<Self, N>,
     N: Noun<A, Self>,
-    Self: Sized,
+    Self: IntoNoun<A, Self, N> + Sized,
 {
     fn get(&self, idx: usize) -> Option<N>;
 
     fn into_parts(self) -> (Option<N>, Option<N>);
-
-    fn into_noun(self) -> N;
 }
