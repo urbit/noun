3509b5b67daea4314e196ea55339b39c841cdb2a
Peter McEvoy <peter@tlon.io>
2022-06-15 13:07:13 -0700

    Better specify Jam and Cue traits

--- src/serdes.rs
+++ src/serdes.rs
@@ -1,3 +1,8 @@
+/// Jamming a type involves two steps:
+/// (1) Convert the implementing type into a noun.
+/// (2) Serialize the noun from (1).
+///
+/// If the implementing type also implements Noun, then (1) is trivial.
 pub trait Jam
 where
     Self: Sized,
@@ -7,6 +12,11 @@ where
     fn jam(self) -> Result<Vec<u8>, Self::Error>;
 }
 
+/// Cueing a type involves two steps:
+/// (1) Deserialize the jammed value into a noun.
+/// (2) Convert the noun from (1) into the implementing type.
+///
+/// If the implementing type also implements Noun, then (2) is trivial.
 pub trait Cue
 where
     Self: Sized,
