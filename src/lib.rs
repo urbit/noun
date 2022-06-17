34d1f16dbced81daf9eb23995918010d2af635a1
Peter McEvoy <peter@tlon.io>
2022-06-17 12:03:19 -0700

    Remove Cue::decode_len()

--- src/lib.rs
+++ src/lib.rs
@@ -91,26 +91,23 @@ where
         }
     }
 
-    /// Read the length in bits of an atom or backreference, returning (bit length, bits read).
-    fn decode_len(src: &mut impl BitRead) -> Result<(u64, u32), ()> {
-        let len_of_len = src.read_unary0().expect("count high bits");
-        // Length must be 63 bits or less.
-        if len_of_len >= u64::BITS {
-            todo!("too large")
-        }
-
-        let len: u64 = src.read(len_of_len).expect("get length");
-        // Most significant bit of the length is always one and always omitted, so add it back now.
-        let len = (1 << len_of_len) | len;
-
-        let bits_read = 2 * len_of_len + 1;
-
-        Ok((len, bits_read))
-    }
-
     /// Decode an atom, returning (bytes, bits read).
     fn decode_atom(src: &mut impl BitRead) -> Result<(Vec<u8>, u32), ()> {
-        let (mut bit_len, mut bits_read) = Self::decode_len(src)?;
+        // Decode the atom length.
+        let (mut bit_len, mut bits_read) = {
+            let len_of_len = src.read_unary0().expect("count high bits");
+            // Length must be 63 bits or less.
+            if len_of_len >= u64::BITS {
+                todo!("too large")
+            }
+
+            let len: u64 = src.read(len_of_len).expect("get length");
+            // Most significant bit of the length is always one and always omitted, so add it back now.
+            let len = (1 << len_of_len) | len;
+
+            let bits_read = 2 * len_of_len + 1;
+            (len, bits_read)
+        };
 
         // This will allocate an extra byte when bit_len is a multiple of u8::BITS, but it's worth it
         // to omit a branch.
